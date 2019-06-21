8477bfa12b1d337d52bbb92b219dafefc34ed7cb
Egil Hansen <egil@assimilated.dk>
2019-06-21 11:31:28 +0000

    SrOnly component unit tests

---
+++ tests/Egil.RazorComponents.Bootstrap.Tests/Components/Accessibility/SrOnlyTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using Org.XmlUnit.Diff;
+using Xunit;
+using Xunit.Abstractions;
+using Egil.RazorComponents.Bootstrap.Extensions;
+using Microsoft.AspNetCore.Components.RenderTree;
+using Microsoft.AspNetCore.Components;
+
+namespace Egil.RazorComponents.Bootstrap.Components.Accessibility
+{
+
+    public class SrOnlyTests : BootstrapComponentBaseTests
+    {
+        [Fact(DisplayName = "SrOnly does not render anything when ChildContent is null")]
+        public void MyTestMethod()
+        {
+            var expectedHtml = string.Empty;
+
+            var result = RenderComponent<SrOnly>(ParameterCollection.Empty);
+
+            result.ShouldBe(expectedHtml);
+        }
+
+        [Fact(DisplayName = "SrOnly renders if ChildContent is not null")]
+        public void SrOnlyRenderCorrectlysIfChildContentIsNotNull()
+        {
+            var content = "CONTENT";
+            var expectedHtml = $@"<span class=""sr-only"">{content}</span>";            
+
+            var result = RenderComponent<SrOnly>(builder => builder.AddContent(content));
+
+            result.ShouldBe(expectedHtml);
+        }
+    }
+}
---
+++ tests/Egil.RazorComponents.Bootstrap.Tests/Components/ShouldlyRazorComponentTestExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Components.Rendering;
+using Org.XmlUnit.Builder;
+using Shouldly;
+
+namespace Egil.RazorComponents.Bootstrap.Components
+{
+    internal static class ShouldlyRazorComponentTestExtensions
+    {
+        private const string Tab = "\t";
+        private const string TestRootElementName = "BootstrapDotNetComponentTest";
+
+        internal static void ShouldBe(this ComponentRenderedText componentRenderedText, string expectedHtml)
+        {
+            var testHtml = string.Concat(componentRenderedText.Tokens).Trim();
+            var test = Input.FromString(WrapInTestRoot(testHtml)).Build();
+            var control = Input.FromString(WrapInTestRoot(expectedHtml)).Build();
+            var diffResult = DiffBuilder.Compare(control).IgnoreWhitespace().WithTest(test).Build();
+
+            var assertDiffMessage = diffResult.Differences
+                .Select(diff => $"- {diff.ToString()}")
+                .Aggregate(string.Empty, (acc, diff) => $"{acc}{Environment.NewLine}{diff}");
+
+            diffResult.HasDifferences().ShouldBeFalse($"should be" +
+                $"{Environment.NewLine}{Environment.NewLine}{expectedHtml}" +
+                $"{Environment.NewLine}{Environment.NewLine}{Tab}but was" +
+                $"{Environment.NewLine}{Environment.NewLine}{testHtml}" +
+                $"{Environment.NewLine}{Environment.NewLine}{Tab}with the following differences:" +
+                $"{Environment.NewLine}{assertDiffMessage}" +
+                $"{Environment.NewLine}");
+        }
+
+        private static string WrapInTestRoot(string html)
+        {
+            return $"<{TestRootElementName}>{Environment.NewLine}{html ?? string.Empty}{Environment.NewLine}</{TestRootElementName}>";
+        }
+    }
+}
